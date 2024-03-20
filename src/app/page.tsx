20e25c262301d9f771e2f5894fe27fb0be015767
NadzeyaHope <radkovanada13@gmail.com>
2024-03-20 19:16:28 +0300

    small fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Container from '@/app/shared/Container';
 import Header from '@/app/widgets/Header';
 import Content from '@/app/widgets/Content';
+import { Image } from '@nextui-org/image';
 
 const Home = () => {
 	return (
@@ -9,8 +10,8 @@ const Home = () => {
 			<Header/>
 			<div className={'flex justify-between'} >
 				<Content />
-					<img
-						className={'w-[275px] bottom-0 fixed right-[-80px] z-[1] lg:mt-auto lg:relative lg:h-[808px] lg:w-[525px] bg-fixed'}
+					<Image
+						className={'w-[275px] bottom-0 md:relative md:w-[625px] md:mt-[100%] fixed right-[-80px] z-[1]'}
 						alt={'mentor'}
 						src={'/mentor2.png'}
 					/>
